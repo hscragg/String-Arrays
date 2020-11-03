8a9dbbaa059832e2e25c29ca8f1af21770ceb6e3
hscragg <hayden.scragg@patelhs.org>
2020-11-03 15:23:44 +0000

    added 1 to the value

--- Main.java
+++ Main.java
@@ -2,14 +2,14 @@ class Main {
   public static void main(String[] args) {
     String[] names = {"Jamal", "Hayden", "Anthony", "Ian", "Gabby", "Dimitrius Damarcus Bartholomew James the Third Jr", "Jaquavious", "Marquavious", "Jumar", "Reeda", "Jeffary"};
 
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < 11; i++){
       System.out.println( names[i]);
     }
 
     names[1] = "Andrew";
 
     System.out.println("\n Here is the updated reverse array:\n");
-    for(int i = 9; i < 10 && i >= 0; i--){
+    for(int i = 9; i < 11 && i >= 0; i--){
       System.out.println( names[i]);
     }
   }
